ff2f5a5d1ae955ca2a0d49786f6bde2744e03568
SHIMIZU Taku <shimizu.taku@gmail.com>
2015-11-18 23:12:45 +0900

    Three strikes and we need refactor functional test

--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,8 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
-def home_page(request):
-    return render(request, 'home.html')
+def home_page(request: HttpRequest) -> HttpResponse:
+    return render(request, 'home.html',
+                  {'new_item_text': request.POST.get('item_text', '')})
