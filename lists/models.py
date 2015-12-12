ff3a121995247af2b77862538edb03e5786489e8
SHIMIZU Taku <shimizu.taku@gmail.com>
2015-12-12 15:43:41 +0900

    Add functional tests

--- lists/models.py
+++ lists/models.py
@@ -1,4 +1,13 @@
 from django.db import models
 
+
 class Item(models.Model):
     text = models.TextField(default='')
+    try:
+        pass
+    except Exception as e:
+        raise
+    else:
+        pass
+    finally:
+        pass
